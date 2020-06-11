aa1bd303f11f70bfbf6be4a2f5fa30939e245863
Johannes ter Haak <jojo@terhaak.de>
2020-06-11 21:00:24 +0200

    init app source

---
+++ src/components/App.vue
@@ -0,0 +1,28 @@
+<script>
+import HelloWorld from './HelloWorld.vue';
+
+export default {
+    name: "App",
+    components: { HelloWorld },
+    
+    props:{
+    },
+    
+    data: function(){ 
+        return {
+        }; 
+    },
+    
+    computed:{
+    },
+}
+</script>
+
+<template>
+    <div>
+        <hello-world></hello-world>
+    </div>
+</template>
+
+<style>
+</style>
