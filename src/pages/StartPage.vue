7aca8c17e4df48ef769742a73d151269de4650cd
Jan Unterbrink <jan.unterbrink@subpath.de>
2020-06-14 21:27:52 +0200

    added menu with all rooms (quick cleaning)

--- src/pages/StartPage.vue
+++ src/pages/StartPage.vue
@@ -3,22 +3,41 @@
 export default {
     name: "StartPage",
     components: {  },
-    
+
     props:{
     },
-    
-    data: function(){ 
-        return {
-        }; 
+    watch: {
+        '$route': 'fetchData'
     },
-    
-    computed:{
+
+    created () {
+        this.fetchData()
     },
+
+    methods:{
+        fetchData(){
+            this.$store.dispatch('load_full_room_list');
+        }
+    },
+
+    data: () => ({
+    }),
+
 }
 </script>
 
 <template>
-    <div>
+    <div id="left">
+        <v-menu bottom offset-y>
+            <template v-slot:activator="{ on, attrs }">
+             <v-btn class="ma-2" v-bind="attrs" v-on="on">quick cleaning</v-btn>
+            </template>
+            <v-list>
+                <v-list-item v-for="room in $store.state.roomList" :key="i" @click="() => {}">
+                    <v-list-item-title>{{ room.name }}</v-list-item-title>
+                </v-list-item>
+            </v-list>
+        </v-menu>
         <v-btn to="/floors">robots and rooms</v-btn>
         <v-btn to="/floors">timetables</v-btn>
         <v-btn to="/floors">templates</v-btn>
