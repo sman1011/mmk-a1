67d3c1c6d9e37cea0e00bd831625ed03fdda862a
Jan Unterbrink <jan.unterbrink@subpath.de>
2020-06-15 23:58:25 +0200

    load self defined events

--- src/pages/TimeTable.vue
+++ src/pages/TimeTable.vue
@@ -53,6 +53,7 @@
                         @click:more="viewDay"
                         @click:date="viewDay"
                         @change="updateRange"
+                        @load="updateRange"
                         @mousedown:event="startDrag"
                         @mousedown:time="startTime"
                         @mousemove:time="mouseMove"
@@ -102,7 +103,13 @@
 
 
 <script>
+    import { store } from '../Store.js'
+
     export default {
+        watch: {
+            '$route': 'fetchData'
+        },
+
         data: () => ({
             focus: '',
             type: 'month',
@@ -124,9 +131,11 @@
             createStart: null,
             extendOriginal: null,
         }),
+
         mounted() {
             this.$refs.calendar.checkChange()
         },
+
         methods: {
             viewDay({date}) {
                 this.focus = date
@@ -149,6 +158,10 @@
                 this.$refs.calendar.next()
             },
 
+            fetchData() {
+                store.dispatch('load_date_list');
+            },
+
             showEvent({nativeEvent, event}) {
                 const open = () => {
                     this.selectedEvent = event
@@ -167,29 +180,39 @@
             },
 
             updateRange({start, end}) {
-                const events = []
-
+                this.fetchData()
                 const min = new Date(`${start.date}T00:00:00`)
                 const max = new Date(`${end.date}T23:59:59`)
-                const days = (max.getTime() - min.getTime()) / 86400000
-                const eventCount = this.rnd(days, days + 20)
+                const days = (max.getTime() - min.getTime()) / 86400000;
 
-                for (let i = 0; i < eventCount; i++) {
-                    const allDay = this.rnd(0, 3) === 0
-                    const firstTimestamp = this.rnd(min.getTime(), max.getTime())
-                    const first = new Date(firstTimestamp - (firstTimestamp % 900000))
-                    const secondTimestamp = this.rnd(2, allDay ? 288 : 8) * 900000
-                    const second = new Date(first.getTime() + secondTimestamp)
+                let events = [];
+                store.state.dateList.forEach((date) => {
+                    console.log(date.id);
+                    console.log(start.date)
+                    /*
+                    if(date.start > end.date) {
+                        return;
+                    }
 
-                    events.push({
-                        name: this.names[this.rnd(0, this.names.length - 1)],
-                        start: first,
-                        end: second,
-                        color: this.colors[this.rnd(0, this.colors.length - 1)],
-                        timed: !allDay,
-                    })
-                }
+                    if(date.end !== null && date.end < start.date) {
+                        return;
+                    }*/
 
+                    switch (date.repeat) {
+                        case "daily":
+                        case "weekly":
+                        default:
+                            events.push({
+                                name: date.name,
+                                start: date.startDate + 'T' + date.begin,
+                                end: date.startDate + 'T' + date.end,
+                                color: date.color,
+                            })
+                    }
+
+                })
+
+                console.log(events)
                 this.events = events
             },
 
