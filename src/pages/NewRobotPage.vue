e9063c88bcadefec3500737ce3be66845758a437
Tim F <hanso.hansen97@gmail.com>
2020-06-17 22:39:58 +0200

    Disable watcher to prevent errors

--- src/pages/NewRobotPage.vue
+++ src/pages/NewRobotPage.vue
@@ -1,53 +1,58 @@
 <script>
 
-export default {
-    name: "NewRobotPage",
-    components: { },
-    
-    props:{
-    },
-    
-    data(){ 
-        return {
-            name: "",
-            floor: 0,
-        }; 
-    },
-    
-    computed:{
-        floorSelectItems(){
-            return this.$store.state.floorList.map( f => ({ text: f.name, value: f.id }) );
+    export default {
+        name: "NewRobotPage",
+        components: { },
+
+        props:{
         },
-    },
+
+        data(){
+            return {
+                name: "",
+                floor: 0,
+                snackbar: false
+            };
+        },
+
+        computed:{
+            floorSelectItems(){
+                return this.$store.state.floorList.map( f => ({ text: f.name, value: f.id }) );
+            },
+        },
 
     watch: {
         '$route': 'onClear'
     },
 
-    created () {
-        this.onClear();
-    },
-    
-    methods:{
-        fetchData(id){
-            this.$store.dispatch('load_floor_list');
+        created () {
+            this.onClear();
         },
-        onSave(){
-            let robot = {
-                name: this.name,
-                floor: this.floor,
-            };
-            this.$store.dispatch('add_new_robot', robot).then( () => {
-                this.$router.go(-1);
-            });
-        },
-        onClear(){
-            this.name = "";
-            this.floor = this.$store.state.floor.id;
-            this.fetchData();
-        },
+
+        methods:{
+            fetchData(id){
+                this.$store.dispatch('load_floor_list');
+            },
+            onSave(){
+                if(this.name && this.floor) {
+                    let robot = {
+                        name: this.name,
+                        floor: this.floor,
+                    };
+                    this.$store.dispatch('add_new_robot', robot).then(() => {
+                        this.$router.go(-1);
+                    });
+                } else{
+                    this.snackbar=true;
+                }
+            },
+            onClear(){
+                this.name = "";
+                this.floor = this.$store.state.floor.id;
+                this.fetchData();
+            },
+        }
     }
-}
 </script>
 
 <template>
@@ -78,6 +83,12 @@ export default {
                 
             </v-col>
         </v-row>
+            <!--<v-btn dark @click="snackbar = true">Open Snackbar</v-btn>-->
+        <v-snackbar
+                v-model="snackbar"
+        >
+            <span>Please enter a valid name.</span>
+        </v-snackbar>
     </v-container>
 </template>
 
